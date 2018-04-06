2afa353ab2ca2ea850ca86cbaa2535fff3f70271
weegwangjin <wgj2050@naver.com>
2018-04-06 15:51:52 +0900

    Connecting python server, App sends voice model, text and server makes audio file

---
+++ src/Android_App/app/src/main/java/kr/seon/alarm/AlarmNotification.java
@@ -0,0 +1,202 @@
+package kr.seon.alarm;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AlarmNotification extends Activity
+{
+    private final String TAG = "Alarm";
+    private Ringtone mRingtone;
+    private Vibrator mVibrator;
+    private final long[] mVibratePattern = { 0, 500, 500 };
+    private boolean mVibrate;
+    private Uri mAlarmSound;
+    private long mPlayTime;
+    private Timer mTimer = null;
+    private Alarm mAlarm;
+    private DateTime mDateTime;
+    private TextView mTextView;
+    private PlayTimerTask mTimerTask;
+    private ImageView imageView;
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+        Intent n_intent = getIntent();
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        setContentView(R.layout.activity_alarm_notification);
+
+        mDateTime = new DateTime(this);
+        mTextView = (TextView)findViewById(R.id.alarm_title_text);
+        imageView = (ImageView)findViewById(R.id.img_voice);
+        //차후에 목소리 모델이 2이상 준비 되면 이미지를 그 모델의 사진으로 교체할 예정
+        imageView.setImageResource(R.drawable.img_inna);
+
+        readPreferences();
+
+        //알람 종료시 강제 종료 되는 오류 해결
+        //MediaPlayer mediaPlayer = new MediaPlayer(); //위에 MediaPlayer가 있어서 이거 주석처리 했어요.
+
+        //사용자가 정한 메세지로 음성파일이 생성되므로 알람 메세지를 가져온다.
+        String url = "http://203.246.112.106/" + n_intent.getStringExtra("file_name") +".mp3";
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+             mediaPlayer.setDataSource(url);
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
+        try {
+             mediaPlayer.prepare();
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
+        mediaPlayer.start();
+
+        mRingtone = RingtoneManager.getRingtone(getApplicationContext(), mAlarmSound);
+        if (mVibrate)
+            mVibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+        start(n_intent);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "AlarmNotification.onDestroy()");
+
+        stop();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        Log.i(TAG, "AlarmNotification.onNewIntent()");
+
+        addNotification(mAlarm);
+
+        stop();
+        start(intent);
+    }
+
+    private void start(Intent intent)
+    {
+        mAlarm = new Alarm(this);
+        mAlarm.fromIntent(intent);
+
+        Log.i(TAG, "AlarmNotification.start('" + mAlarm.getTitle() + "')");
+
+        mTextView.setText(mAlarm.getTitle());
+
+
+        mTimerTask = new PlayTimerTask();
+        mTimer = new Timer();
+        mTimer.schedule(mTimerTask, mPlayTime);
+        mRingtone.play();
+        if (mVibrate)
+            mVibrator.vibrate(mVibratePattern, 0);
+    }
+
+    private void stop()
+    {
+        Log.i(TAG, "AlarmNotification.stop()");
+
+        mTimer.cancel();
+        mRingtone.stop();
+        mediaPlayer.stop();
+        if (mVibrate)
+            mVibrator.cancel();
+    }
+
+    public void onDismissClick(View view)
+    {
+        stop();
+        finish();
+    }
+
+    private void readPreferences()
+    {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mAlarmSound = Uri.parse(prefs.getString("alarm_sound_pref", "DEFAULT_RINGTONE_URI"));
+        mVibrate = prefs.getBoolean("vibrate_pref", true);
+        mPlayTime = (long)Integer.parseInt(prefs.getString("alarm_play_time_pref", "30")) * 1000;
+    }
+
+    private void addNotification(Alarm alarm)
+    {
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification;
+        PendingIntent activity;
+        Intent intent;
+
+        Log.i(TAG, "AlarmNotification.addNotification(" + alarm.getId() + ", '" + alarm.getTitle() + "', '" + mDateTime.formatDetails(alarm) + "')");
+
+        intent = new Intent(this.getApplicationContext(), MainActivity.class);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        activity = PendingIntent.getActivity(this, (int)alarm.getId(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        notification = builder
+                .setContentIntent(activity)
+                .setSmallIcon(R.drawable.splash)
+                .setAutoCancel(true)
+                .setContentTitle("Missed alarm: " + alarm.getTitle())
+                .setContentText(mDateTime.formatDetails(alarm))
+                .build();
+
+        notificationManager.notify((int)alarm.getId(), notification);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        finish();
+    }
+
+    private class PlayTimerTask extends TimerTask
+    {
+        @Override
+        public void run()
+        {
+            Log.i(TAG, "AlarmNotification.PalyTimerTask.run()");
+            addNotification(mAlarm);
+            finish();
+        }
+    }
+}
--- src/Android_App/app/src/main/java/kr/seon/alarm/AlarmReceiver.java
+++ src/Android_App/app/src/main/java/kr/seon/alarm/AlarmReceiver.java
@@ -1,64 +1,30 @@
 package kr.seon.alarm;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.PowerManager;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
-import android.view.Gravity;
-import android.widget.Toast;
 
-public class AlarmReceiver extends BroadcastReceiver {
-
-    Context context;
+public class AlarmReceiver extends BroadcastReceiver
+{
+    private final String TAG = "Alarm";
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        this.context = context;
-        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"");
-        wakeLock.acquire();
+    public void onReceive(Context context, Intent intent)
+    {
+        Intent newIntent = new Intent(context, AlarmNotification.class);
+        Alarm alarm = new Alarm(context);
 
-        Log.d("alarm", "go");
+        alarm.fromIntent(intent);
+        alarm.toIntent(newIntent);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
-        PendingIntent pendingIntent;
+        Log.i(TAG, "AlarmReceiver.onReceive('" + alarm.getTitle() + "')");
 
-        Toast toast = Toast.makeText(context, "알람 울림", Toast.LENGTH_LONG);
-        toast.setGravity(Gravity.TOP, 0, 200);
-        toast.show();
+        // Sending alarm file name
+        newIntent.putExtra("file_name",alarm.getTitle());
 
-        wakeLock.release();
-
-
-        try {
-            intent = new Intent(context, removeActivity.class);
-            pendingIntent = PendingIntent.getActivity(context, 111, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            Log.d("ServicePending++ :",""+pendingIntent.toString());
-            pendingIntent.send();
-        } catch (PendingIntent.CanceledException e) {
-            e.printStackTrace();
-        }
-        notification();
-    }
-
-    void notification() {
-        Intent intent = new Intent();
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), android.R.drawable.ic_menu_gallery);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,PendingIntent.FLAG_ONE_SHOT);
-
-        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context).setSmallIcon(android.R.drawable.ic_menu_gallery).setLargeIcon(bitmap).setContentTitle("알람").setContentText("알람 ㄸㄹㄸㄹ~").setAutoCancel(true).setSound(soundUri).setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-
+        context.startActivity(newIntent);
     }
 }
